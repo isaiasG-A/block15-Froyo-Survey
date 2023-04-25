c3d2647f890865bf15af593ea73879a18fce970f
Isaias Garcia <isaias@Isaiass-MacBook-Air.local>
2023-04-25 10:49:47 -0700

    new properties have been added to customer object and a for in loop has been created to print the objects keys

--- froyoSurvey.js
+++ froyoSurvey.js
@@ -15,7 +15,7 @@ const customer = {
 customer["email"] = "Jak3Smith1992@email.com";
 customer["phone"] = 3195551234;
 customer["zipCode"] = "63132";
-customer["favoriteFlavors"] = "coffee, strawberry, matcha";
+customer["favoriteFlavors"] =["coffee", "strawberry", "matcha"] ;
 
     //Deleting  properties
 delete customer.zipCode;
@@ -23,6 +23,13 @@ delete customer.favoriteStore;
 
     //Adding properties
 customer["toppings"] = ["chocolate sprinkles", "wafer straws", "gummy bears"];
+customer["futureFlavors"] = "mango";
+customer["todaysPurchaseCost"] = 5.32;
 
+      //for in loop to iterate over object and print the keys
 
-console.table(customer);
+      for(const key in customer) {
+        console.log(key)
+      }
+
+// console.table(customer);
