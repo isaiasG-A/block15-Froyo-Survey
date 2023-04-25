2469dc9f924ee0e99d13086803c7faf1512b926d
Isaias Garcia <isaias@Isaiass-MacBook-Air.local>
2023-04-25 10:41:27 -0700

    specific properties have been modified, others have been deleted and new properties have been added to cutomer object as instructed.

--- froyoSurvey.js
+++ froyoSurvey.js
@@ -1,4 +1,4 @@
-const cutomer = {
+const customer = {
   firstName: "jake",
   lastName: "smith",
   email: "jaekSmih!aol.com",
@@ -9,3 +9,20 @@ const cutomer = {
   favoriteStore: "Target",
   firstVisit: false
 };
+
+
+      //Modifiying properties
+customer["email"] = "Jak3Smith1992@email.com";
+customer["phone"] = 3195551234;
+customer["zipCode"] = "63132";
+customer["favoriteFlavors"] = "coffee, strawberry, matcha";
+
+    //Deleting  properties
+delete customer.zipCode;
+delete customer.favoriteStore;
+
+    //Adding properties
+customer["toppings"] = ["chocolate sprinkles", "wafer straws", "gummy bears"];
+
+
+console.table(customer);
